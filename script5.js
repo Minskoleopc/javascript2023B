e5d54bbaab26290b38688ffef255ca710411efeb
Minskoleopc <minskoleopc@gmail.com>
2023-01-18 20:00:45 +0530

    added

---
+++ script5.js
@@ -0,0 +1,131 @@
+// type 
+
+// Introvert      | Extrovert
+
+// calm           | loud
+// less outing    | more outing
+// less social    |  more social
+
+// Human
+// property - age, weight ,color, height
+// method - talk() , walk() , sleep()
+
+// Vehicle 
+// property - color , model , regNo 
+// Method  - start() , stop()
+
+// Bank 
+// Property - accNo , accName , IFSC code , bal
+// Withdrawl() , deposit()
+
+
+let x = 10
+console.log(x)
+console.log(typeof x) // Number -- 10,-10,10.7,-90.90,0
+
+
+let y = true 
+console.log(y)
+console.log(typeof y) // Boolean true or false
+
+let z = 'chinmay'
+console.log(z)
+console.log(typeof z) //'' , 'A','A12','A1232@sad',"231","@@@"
+
+
+console.log(typeof 10)
+console.log(typeof '10')
+
+// comparison 
+
+// < , > , >= , <=
+// == , !=  only value 
+//=== , !==  value and type
+// entity < entity
+console.log(6 == 6) // true
+console.log(6 != 4) // true
+console.log(6 == '6') // true
+console.log(6 >= 6) // true
+console.log(6 <= 4) // false
+console.log(4 <= 4) // true
+console.log(4 === 4) // true
+console.log(4 === '4') // false
+console.log(4 !== '4')  // true
+console.log(4 < 89) // true
+console.log(89 > 4) // true
+
+
+// logical operators
+
+// AND && 
+
+// true   &&   true   ======> true
+// false  &&   true   ======> false
+// true   &&   false  ======> false
+// false  &&   false  ======> false 
+
+console.log(7 !== '7' && 8 !== 9)
+console.log(7 === '7' && 8 !== 9)
+console.log(7 === 7 && 8 === 9)
+console.log(7 === '7' && 8 === 9)
+
+// OR  ||
+// true   ||   true   ======> true
+// false  ||   true   ======> true
+// true   ||   false  ======> true
+// false  ||   false  ======> false 
+
+
+console.log(7 == 7 || 8 == 8)
+console.log(7 != 7 || 8 == 8)
+console.log(7 == 7 || 8 != 8)
+console.log(7 != 7 || 8 != 8)
+
+
+// Not  !
+
+//True   --- False 
+// False --- True
+
+console.log(!(9 === 9))
+console.log(!(9 !== 9))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
