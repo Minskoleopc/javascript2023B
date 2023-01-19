7247be106d359ebb981c1b2f9bf5b46f09709702
Minskoleopc <minskoleopc@gmail.com>
2023-01-19 20:00:29 +0530

    added

---
+++ script7.js
@@ -0,0 +1,85 @@
+
+// conditional statements 
+// 1 input ---------- multiple outcomes
+
+
+// numT > 0  && numT <= 5  -----> 10 %
+// numT > 5  && numT <= 10 -----> 20 %
+// numT > 10  ------------------> 30 %
+
+// if(condition){
+//     // statement
+// }
+
+let numT = 60
+
+// if(numT > 0 && numT <= 5){
+//     console.log("10 percent discount")
+// }
+// if(numT > 5 && numT <= 10){
+//     console.log("20 percent discount")
+// }
+// if(numT > 10){
+//     console.log("30 percent discount")
+// }
+
+// if(numT > 0 && numT <= 5){
+//     console.log("10 percent discount")
+// }
+// else if(numT > 5 && numT <= 10){
+//     console.log("20 percent discount")
+// }
+// else if(numT > 10){
+//     console.log("30 percent discount")
+// }
+
+// program 
+let marks = 56
+// if(marks > 90){
+//     console.log('Grade A')
+// }
+// if(marks > 75){
+//     console.log('Grade B')
+// }
+// if(marks > 60){
+//     console.log('Grade C')
+// }
+
+if(marks > 90){
+    console.log('Grade A')
+}
+else if(marks > 75){
+    console.log('Grade B')
+}
+else if(marks > 60){
+    console.log('Grade C')
+}
+else {
+    console.log('Fail')
+}
+
+
+// single line comment 
+/*
+    multiline comment
+*/ 
+
+// program 3
+
+let a = 50;
+let b = 50;
+// if(a > b){
+//     console.log('a is greater')
+// }
+// else {
+//     console.log('b is greater')
+// }
+//condition ? console.log('if true'):console.log('for false')
+a == b ?console.log('both are equal'):console.log('not equal')
+
+
+
+
+
+
+
