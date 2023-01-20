e7fe20b4ab686b3ae5ca2f74fffa75e82ca0109f
Minskoleopc <minskoleopc@gmail.com>
2023-01-20 19:56:43 +0530

    added

---
+++ script8.js
@@ -0,0 +1,77 @@
+// conditional statements
+// input ===> multiple output
+
+
+let numT = 30
+// if(numT > 0 && numT <= 5){
+//     console.log("10 % discount")
+// }
+// if(numT > 5 && numT <= 10){
+//     console.log("20 % discount")
+// }
+// if(numT > 10){
+//     console.log("30 % discount")
+// }
+
+// if(numT > 0 && numT <= 5){
+//     console.log("10 % discount")
+// }
+// else if(numT > 5 && numT <= 10){
+//     console.log("20 % discount")
+// }
+// else if(numT > 10){
+//     console.log("30 % discount")
+// }
+
+let marks = 66
+// if(marks > 90){
+//     console.log("Grade A")
+// }
+// if(marks > 75){
+//     console.log("Grade B")
+// }
+// if(marks > 65){
+//     console.log("Grade C")
+// }
+
+// if(marks > 90){
+//     console.log("Grade A")
+// }
+// else if(marks > 75){
+//     console.log("Grade B")
+// }
+// else if(marks > 65){
+//     console.log("Grade C")
+// }
+
+// program 3
+let a = 10
+let b = 50
+if(a > b){
+    console.log('a is greater')
+}
+else {
+    console.log('b is greater')
+}
+
+// program 4
+//condition ?block for true : block for false
+a > b ? console.log('a is greater'):console.log('b is greater')
+
+
+// program 5
+let age = 17
+let r1 = age >= 18? "can drive":"cannot drive"
+console.log(r1)
+
+// Truthy  -- 1,-34,"a"," ",5==5, 'A', '&' ,{},[]
+// Falsy  -- 0 , null , NaN , undefined ,8 !== 8 , false
+
+if(false){
+    console.log("hello ")
+}
+else {
+    console.log("bye ")
+}
+
+// sat sun
