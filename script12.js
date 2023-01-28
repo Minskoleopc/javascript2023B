d848da010322baf137d12b619bc4c87300408c65
Minskoleopc <minskoleopc@gmail.com>
2023-01-28 10:35:25 +0530

    added

---
+++ script12.js
@@ -0,0 +1,80 @@
+let city = "mumbai"
+
+// switch(city){
+//     case 'pune':
+//         console.log('MH')
+//     case 'indore':
+//         console.log('MP')
+//     case 'jaipur':
+//         console.log('RJ')
+//     case 'kanpur':
+//         console.log('UP')
+//     default:
+//         console.log('In correct city name')
+// }
+
+
+// switch(city){
+//     case 'pune':
+//         console.log('MH')
+//         break
+//     case 'indore':
+//         console.log('MP')
+//         break
+//     case 'jaipur':
+//         console.log('RJ')
+//         break
+//     case 'kanpur':
+//         console.log('UP')
+//         break
+//     default:
+//         console.log('In correct city name')
+// }
+
+// switch(city){
+//     case 'pune':
+//     case 'mumbai':
+//         console.log('MH')
+//         break
+//     case 'indore':
+//     case 'bhopal':
+//         console.log('MP')
+//         break
+//     case 'jaipur':
+//     case 'udaipur':
+//         console.log('RJ')
+//         break
+//     case 'kanpur':
+//     case 'varanasi':
+//         console.log('UP')
+//         break
+//     default:
+//         console.log('In correct city name')
+// }
+
+// program 4
+// loop
+for(let i = 1 ; i <= 3 ; i++){ //2 // 3 // 4
+    console.log(i) // 1 // 2 // 3
+}
+
+// program 5
+// loop
+for(let i = 3 ; i >= 1 ; i--){ //2 // 1 // 0
+    console.log(i) // 3 // 2 // 1
+}
+
+// program 6
+for(let i = 2 ; i <= 20 ; i = i + 2){ // 22
+    console.log(i)
+}
+
+// program 3
+
+
+for(let i = 1 ; i<10 ; i++){
+    if(i == 5){
+        break;
+    }
+    console.log(i)
+}
