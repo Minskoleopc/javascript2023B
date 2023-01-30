503affc204bfec8461145136c4db6bf2d25fd32d
Minskoleopc <minskoleopc@gmail.com>
2023-01-30 21:40:44 +0530

    added

---
+++ script13.js
@@ -0,0 +1,87 @@
+
+// one input and multiple output
+// numT > 0 && numT <= 5 ----> 10 %
+// numT > 5 && numT <= 10 ----> 20 %
+// numT > 10              -----> 30 %
+
+// if(condition){
+//     // statment
+// }
+
+let numT = 30
+// if(numT > 0 && numT <= 5){
+//     console.log('10 % discount')
+// }
+// if(numT > 5 && numT <= 10){
+//     console.log('20 % discount')
+// }
+// if(numT > 10){
+//     console.log('30 % discount')
+// }
+
+// if(numT > 0 && numT <= 5){
+//     console.log('10 % discount')
+// }
+// else if(numT > 5 && numT <= 10){
+//     console.log('20 % discount')
+// }
+// else if(numT > 10){
+//     console.log('30 % discount')
+// }
+
+//--------------------------------------------->
+
+let marks = 55
+// if(marks > 90){
+//     console.log('Grade A')
+// }
+// if(marks > 75){
+//     console.log('Grade B')
+// }
+// if(marks > 60){
+//     console.log('Grade C')
+// }
+
+// if (marks > 90) {
+//     console.log('Grade A')
+// }
+// else if (marks > 75) {
+//     console.log('Grade B')
+// }
+// else if (marks > 60) {
+//     console.log('Grade C')
+// }
+// else {
+//     console.log('Failed please try again')
+// }
+
+// Tenary operator
+let a = 100
+let b = 50
+
+// if(a > b){
+//     console.log('a is greater')
+// }
+// else{
+//     console.log('b is greater')
+// }
+// a > b ? console.log('a is greater') : console.log('b is greater')
+
+// let age = 18
+// let a1 = age >= 18 ? "can drive":"cannot drive"
+// console.log(a1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
