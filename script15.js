940eaab25954a4cd8d91c2fe528742f0184ff0aa
Minskoleopc <minskoleopc@gmail.com>
2023-01-31 19:59:08 +0530

    added

---
+++ script15.js
@@ -0,0 +1,71 @@
+// type 
+
+// Human 
+// Property --  age , weight , height ,color , gender
+// Method -- walk() , talk() 
+
+// Vehicle 
+// Property - color , modelNo , regNo ,type
+// Method start() stop()
+
+// Bank 
+// Property - accNo , accName, accType , bal 
+// Method - deposit() , withdrawl()
+
+
+// Object --- property and method
+// Method --- Gym
+// action - excercise
+// return health
+
+
+let x = 10
+console.log(x)
+
+// Array ------
+//              0         1          2       3
+let names = ["chinmay","mayuri","sarika","poorva"]
+console.log(names[0])
+console.log(names[1])
+console.log(names.length)
+
+//              0   1   2   3   4   5   6   7   8   9
+let numbers = [111,222,333,444,555,666,777,888,999,000]
+console.log(numbers[0])
+console.log(numbers[1])
+// Object 
+// Property ---
+// Methods
+console.log(numbers.length)
+
+for(let i = 0 ; i < numbers.length ; i++){
+    console.log(i)
+    console.log(numbers[i])
+
+    // i    --- 0 
+    // i    --- 1
+    // i    ----2
+    // i    --- 3
+    // i    --- 4
+    // i    --- 5
+    // i    --- 6
+    // i    --- 7
+    // i    --- 8
+    // i    --- 9
+}
+
+//              0         1       2       3
+let fruits = ["apple","banana","grapes","mango"]
+for(let i = 0 ; i < fruits.length ; i++){
+    //console.log(i)
+    console.log(fruits[i])
+}
+
+// More than store  ----- Array
+// Array is object 
+// Properties and Values
+// Array stores the value by index
+// Index starts from 0
+// Length - 1 is always last index
+
+
