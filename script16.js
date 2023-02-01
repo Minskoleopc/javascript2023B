37afe3cdf2f83567aa029506deafc344f3c9f9cd
Minskoleopc <minskoleopc@gmail.com>
2023-02-01 20:35:36 +0530

    added

---
+++ script16.js
@@ -0,0 +1,24 @@
+// Object 
+
+// Property and methods
+// method  action and return 
+
+//              0        1        2        3       4
+let names = ["chinmay","sarika","poorva","minal","amol"]
+console.log(names[0])
+console.log(names[1])
+console.log(names[names.length-1])
+// length -1 is always last index
+
+//          0         1        2        3      4
+names = ["chinmay","sarika","poorva","minal","amol"]
+
+console.log(names[0])
+
+for(let i = 0 ; i < names.length ; i++){
+    //console.log(i)
+    // i ====> 0
+    // i ====> 1
+    // i ====> 2
+    // i ====> 3
+}
