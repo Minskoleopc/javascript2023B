6d00f968602846d664d233c7b18026999ce6c2f9
Minskoleopc <minskoleopc@gmail.com>
2023-02-25 19:58:02 +0530

    added

---
+++ DOM/script.js
@@ -0,0 +1,12 @@
+
+// html --- hypertext marup language
+// css  --- cascading style sheet
+// js  ---- javascript
+
+
+document.querySelector('h1').addEventListener('click',function(){
+    document.querySelector('h1').textContent =  document.querySelector('h1').textContent.toUpperCase();
+    document.querySelector('h1').style.color = "yellow";
+    document.querySelector('h1').style.backgroundColor = "blue";
+})
+
