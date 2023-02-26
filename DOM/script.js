a7d8fb501ae30d5637b7d91f2293614bcf95381a
Minskoleopc <minskoleopc@gmail.com>
2023-02-26 11:48:46 +0530

    added

--- DOM/script.js
+++ DOM/script.js
@@ -3,7 +3,6 @@
 // css  --- cascading style sheet
 // js  ---- javascript
 
-
 document.querySelector('h1').addEventListener('click',function(){
     document.querySelector('h1').textContent =  document.querySelector('h1').textContent.toUpperCase();
     document.querySelector('h1').style.color = "yellow";
